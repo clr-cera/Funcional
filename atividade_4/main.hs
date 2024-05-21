c385399a851e8455e8b59b6334ba4421fcb61a2d
RenanScarpin <renanparpinelliscarpin@gmail.com>
2024-05-21 14:09:37 -0300

    Working on 20/20 cases now

--- atividade_4/main.hs
+++ atividade_4/main.hs
@@ -16,13 +16,16 @@ main = do
 
 --Função que retorna a string da contagem de pontos formatado conforme as specificações
 contagem :: [Int] -> String
-contagem [x1,x2,x3]
+contagem [x1,x2,x3] -- Ultimas 3 jogadas
     | x1 == 10      = "X " ++ contagem [x2,x3]
-    | x1 + x2 == 10 = show x1 ++ " / " ++ show x3 ++ " | "
+    | x1 + x2 == 10 = showl x1 ++ " / " ++ showl x3 ++ " | "
+contagem [x1,x2] -- Ultimas 2 jogadas
+    | x1 + x2 == 10 = showl x1 ++ " / | " --Caso seja um Spare
+    | otherwise     = showl x1 ++ " " ++ showl x2 ++ " | "--Caso não seja um spare
 contagem (10:xs) = "X _ | " ++ contagem xs
 contagem (x1:x2:xs)
-    | x1 + x2 == 10 = show x1 ++ " / | " ++ contagem xs
-    | otherwise     = show x1 ++ " " ++ show x2 ++ " | " ++ contagem xs
+    | x1 + x2 == 10 = showl x1 ++ " / | " ++ contagem xs
+    | otherwise     = showl x1 ++ " " ++ showl x2 ++ " | " ++ contagem xs
 contagem _ = ""
 
 --Função que calcula a pontuação da partida de boliche
@@ -33,3 +36,8 @@ pontuacao (x1:x2:x3:xs)
     | x1 == 10       = x1 + x2 + x3 + pontuacao (x2:x3:xs) --strike
     | x1 + x2 == 10  = x1 + x2 + x3 + pontuacao (x3:xs) --spare
     | otherwise      = x1 + x2 + pontuacao (x3:xs)
+
+--Mistura de show com bowl, mostra os números como apareciam na tabela de pontos do Boliche
+showl :: Int -> String
+showl 10 = "X"
+showl x = show x
